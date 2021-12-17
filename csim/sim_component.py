9adc97f68aecea53509291887a37d4d88ad5d7c0
JObog <jobogbaimhe@gmail.com>
2021-12-16 22:41:09 -0500

    removed pinMap from sim comp class

--- csim/sim_component.py
+++ csim/sim_component.py
@@ -6,7 +6,6 @@ from typing import Tuple, List, Dict, Optional
 class SimCompoment(BaseComponent):
     def __init__(self, pos: Tuple[int, int], label: str, num_pins: int):
         super().__init__(pos, label)
-        self._pinMap: Dict[str, int] = {}
         self._pins: List[Optional[Pin]] = [None] * num_pins
 
     def setPin(self, pin: Pin, idx: int):
@@ -14,9 +13,3 @@ class SimCompoment(BaseComponent):
 
     def getPin(self, idx: int) -> Pin: 
         return self._pins[idx]
-
-    def resolvedAlias(self, alias: str) -> int:
-        return self._pinMap.get(alias, -1)
-
-    def aliasPin(self, alias: str, idx: int):
-        self._pinMap[alias] = idx 
