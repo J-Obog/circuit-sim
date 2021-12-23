f2cbd29727df84f7c753bacd3fa82d68ffe77cce
JObog <jobogbaimhe@gmail.com>
2021-12-22 20:49:06 -0500

    added bounds attribute to component class

--- csim/base_component.py
+++ csim/base_component.py
@@ -1,14 +1,24 @@
+from _typeshed import Self
 from typing import Tuple
 
 class BaseComponent:
-    def __init__(self, pos: Tuple[int, int], label: str):
-        self._x: int = pos[0]
-        self._y: int = pos[1]
+    def __init__(self, x: int, y: int, w: int, h: int, label: str):
+        self._x: int = x
+        self._y: int = y
+        self._w: int = w
+        self._h: int = h
         self._label: str = label
 
-    def setPos(self, pos: Tuple[int, int]):
-        self._x = pos[0]
-        self._y = pos[1]
+    def setBounds(self, w: int, h: int):
+        self._w = w
+        self._h = h
+
+    def getBounds(self) -> Tuple[int, int]:
+        return (self._w, self._h)
+
+    def setPos(self, x: int, y: int):
+        self._x = x
+        self._y = y
 
     def getPos(self) -> Tuple[int, int]:
         return (self._x, self._y)
--- csim/simulation.py
+++ csim/simulation.py
@@ -1,12 +1,15 @@
 from csim.sim_component import SimCompoment
 from csim.gen import UidGenerator
-from typing import Dict
-
+from typing import Dict, Optional, List, Tuple
 
 class Simulation:
     def __init__(self):
         self.__compMap: Dict[str, SimCompoment] = {}
+        self.__connections: List[Tuple[str, str]] = [] 
 
     def addComponent(self, component: SimCompoment):
         uid = UidGenerator().generate_uid()
-        self.__compMap[uid]
+        self.__compMap[uid] = component
+
+    def getComponent(self, uid: str) -> Optional[SimCompoment]:
+        return self.__compMap(uid, None)
